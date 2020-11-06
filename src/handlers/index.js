8541ec1bb4782fd4fbec59bdb7566f1f38db5fa1
brettsodie <blsodie@gmail.com>
2020-11-05 20:45:39 -0500

    feat: add queue for incoming audio

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,8 +1,14 @@
 const audio = require('../utilities/audio');
+const queue = require('../utilities/queue');
 
 const playAudioHandler = file => {
   console.log(file);
-  audio.downloadAndPlay(file);
+  if (queue.length) {
+    queue.push(file);
+  } else {
+    queue.push(file);
+    audio.downloadAndPlay(file);
+  }
 };
 
 const stopAudioHandler = () => {
--- src/utilities/audio.js
+++ src/utilities/audio.js
@@ -1,17 +1,29 @@
 const { exec } = require('child_process')
 const fs = require('fs');
 const https = require('https');
+const queue = require('./queue');
 /* must $sudo apt-get install mpg123 */
 
 const downloadAndPlay = (uri) => {
   const file = fs.createWriteStream('./audio.mp3');
   https.get(uri, res => {
     res.pipe(file);
-    file.on('finish', () => play('./audio.mp3'));
+    file.on('finish', () => {
+      play('./audio.mp3', () => {
+      })
+    })
   });
 };
 
-const play = path => exec(`mpg123 ${path}`);
+const play = path => {
+  exec(`mpg123 ${path}`, () => {
+    console.log('done');
+    queue.shift();
+    if (queue.length) {
+      downloadAndPlay(queue[0]);
+    }
+  });
+}
 
 const stop = () => exec('pkill mpg123');
 
